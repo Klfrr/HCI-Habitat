88ae2416fce8500b693cfeadc4ca74eaca405c30
Klfrr <kayjiayi495@gmail.com>
2023-05-10 00:35:38 -0700

    MythicScript update
    Time not working yet

--- Assets/Scripts/MythicScript.cs
+++ Assets/Scripts/MythicScript.cs
@@ -18,8 +18,9 @@ public class MythicScript : MonoBehaviour
 
     // 
     public int points = 0;
-    public Text time;
-    public Text date;
+    public Text timeLabel;
+    public Text dateLabel;
+    public int repetitions;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +31,7 @@ public class MythicScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Updatetime();
     }
 
     void checkTime()
@@ -40,6 +41,17 @@ public class MythicScript : MonoBehaviour
         string timeUS = System.DateTime.UtcNow.ToLocalTime().ToString("M/d/yy   hh:mm tt");
         largeText.text = time + "[breaktag]" + timeUS;
         */
+    }
+
+    public IEnumerator Updatetime()
+    {
+        while(true)
+            {
+                var today = System.DateTime.Now;
+                dateLabel.text = today.ToString("MM-dd-yyyy");
+                timeLabel.text = today.ToString("HH:mm");
+                yield return new WaitForSeconds(0.2f); 
+            }
     }
 
     public void addHappiness(float num = dHappiness)
@@ -70,6 +82,23 @@ public class MythicScript : MonoBehaviour
     void removeThirst(float num = dThirst)
     {
         thirst -= num;
+    }
+
+    void deathScenario(float stat)
+    {
+        if(stat <= 0)
+        {
+            //move screen to death screen
+        }
+    }
+
+    void releaseScenario(int growth)
+    {
+        if(growth == repetitions)
+        {
+            //give Player money
+            //move screen to Catalog screen
+        }
     }
 
 }
