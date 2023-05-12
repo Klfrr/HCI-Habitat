81cc5503beef493fb39f8ba04f508e16777da47d
Abida Mim <amim@nevada.unr.edu>
2023-05-12 11:36:46 -0700

    habit saves to scriptable object

--- Assets/Scripts/SetHabit.cs
+++ Assets/Scripts/SetHabit.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 
 // Abida Mim
 // when a user types in a habit, it appears on screen
+
 public class SetHabit : MonoBehaviour
 {
-    public Text habit_input;
+    public Text habit_output;
     public InputField input_box;
 
-   public void setHabitName()
+    public UserInfo savedData;
+
+    public void setHabitName()
     {
-        habit_input.text = input_box.text;
+        savedData.habitString = input_box.text;
+        habit_output.text = savedData.habitString;
 
     }
 }
